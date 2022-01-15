2e22131ce6fa16c93a562e85c650dbf7ecdce4c4
takai <nipas2013@gmail.com>
2022-01-15 21:52:31 +0700

    add mock data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from './components/Card';
+import { robots } from './data/robots';
 import './App.css';
 import 'tachyons';
 
@@ -8,7 +9,14 @@ class App extends React.Component {
   render () {
     return (
       <div className="App">
-        <Card />
+        {
+          robots.map(data => {
+           return <Card 
+                    name={ data.name }
+                    email={ data.email }
+                  />
+          })
+        } 
       </div>
     );
   }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,18 @@
 import React from "react";
 
 class Card extends React.Component {
+
+    constructor (props) {
+        super(props);
+    }
+
     render () {
         return (
             <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-                <img src="https://robohash.org/test" alt="robots"></img>
+                <img src={`https://robohash.org/${ this.props.name }`} alt="robots"></img>
                 <div className="tc">
-                    <h2>Jane Doe</h2>
-                    <p>jane@email.com</p>
+                    <h2>{ this.props.name }</h2>
+                    <p>{ this.props.email }</p>
                 </div>
             </div>
         );
