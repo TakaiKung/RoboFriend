98f64ac1c3db66c0e1d7b44943818793f629c7b0
takai <nipas2013@gmail.com>
2022-01-16 18:45:12 +0700

    add scroll component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import CardList from './components/CardList';
 import SearchBox from './components/SearchBox';
 import './App.css';
 import 'tachyons';
-
+import Scroll from './components/Scroll';
 
 class App extends React.Component {
   constructor() {
@@ -28,13 +28,20 @@ class App extends React.Component {
   render() {
     console.log("render")
     const filterRobots = this.state.robots.filter(robot => robot.name.toLowerCase().includes(this.state.searchField.toLowerCase()))
-    return (
-      <div className='tc'>
-        <h1>RobotFriends</h1>
-          <SearchBox onSearchChange={this.onSearchChange}/>
-          <CardList robots = { filterRobots } />
-      </div>
-    )
+    if (this.state.robots.length < 1) {
+      return <h1 className='tc'>Loading...</h1>
+    }
+    else {
+      return (
+        <div className='tc'>
+          <h1>RobotFriends</h1>
+            <SearchBox onSearchChange={this.onSearchChange}/>
+            <Scroll>
+              <CardList robots = { filterRobots } />
+            </Scroll>  
+        </div>
+      )
+    }
   }
 
 }
