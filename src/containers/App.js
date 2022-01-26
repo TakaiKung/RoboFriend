fc49458fde914a571bd0dec328bff23877a8f277
takai <nipas2013@gmail.com>
2022-01-26 18:06:25 +0700

    create rudux store and manage state of searchField with redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import './App.css';
 import 'tachyons';
-
+import { connect } from 'react-redux';
 import { robots } from '../data/robots';
 import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll' 
 import ErrorBoundary from '../components/ErrorBoundary';
+import { setSearchField } from '../stateManage/action';
+
+const mapStateToProps = state => {
+  console.log(state)
+  return {
+    searchField: state.searchField // --> It come from reducer
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+  }
+}
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       robots : [],
-      searchField : ''
     }
     console.log('Constructor')
   }
@@ -23,19 +36,16 @@ class App extends React.Component {
     console.log('componentDidmount')
   }
 
-  onSearchChange = e => {
-    this.setState({ searchField : e.target.value })
-  }
-
   render() {
     console.log("render")
-    const { robots, searchField } = this.state;
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
     const filterRobots = robots.filter(robot => robot.name.toLowerCase().includes(searchField.toLowerCase()))
     return !robots.length ? 
       <h1 className='tc'>Loading...</h1> :
       <div className='tc'>
         <h1>RobotFriends</h1>
-          <SearchBox onSearchChange={this.onSearchChange}/>
+          <SearchBox onSearchChange={onSearchChange}/>
           <ErrorBoundary>
             <Scroll>
               <CardList robots = { filterRobots } />
@@ -45,4 +55,4 @@ class App extends React.Component {
   }
 
 }
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { searchRobots } from './stateManage/reducer';
+
+const store = createStore(searchRobots);
 
 ReactDOM.render(
   <React.StrictMode>
-   <App />
+    <Provider store={ store }>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
